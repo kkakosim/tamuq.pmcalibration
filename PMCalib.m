507bfea38333b9a617fadc60c9984389681eafb0
kkakosim <kkakosim@gmail.com>
2017-06-10 13:22:04 +0300

    update

--- PMCalib.m
+++ PMCalib.m
@@ -1,14 +1,66 @@
 function PMCalib()
 load('aqCalib.mat');
 Diam=[0.265 0.290 0.325 0.375 0.425 0.475 0.540 0.615 0.675 0.750 0.900 1.150 1.450 1.800 2.250 2.750 3.250 3.750 4.500 5.750 7.000 8.000 9.250]';
-Dens=[2750 2750 2750 2750 2750 2750 2750 2750 2750 2750 2750 2850 2850 2850 2850 2180 2180 2180 2180 2180 2180 2180 2180]'; 
+Dens=[2690 2690 2690 2690 2690 2690 2690 2690 2690 2690 2690 2860 2860 2860 2860 2305 2305 2305 2305 2305 2305 2305 2305]'; 
+Dens=[2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340 2340]'; 
+
+
+%PM1mod=aqCalibNUM(:,1:11)*Mass(1:11);
+[D(3),err(3)]=getD(Dens(1:11),Diam(1:11),aqCalibNUM(:,1:11),aqCalibPM(:,3)); %PM1
+[D(2),err(2)]=getD(Dens(12:15),Diam(12:15),aqCalibNUM(:,12:15),aqCalibPM(:,2)-aqCalibPM(:,3)); %PM2.5
+[D(1),err(1)]=getD(Dens(16:23),Diam(16:23),aqCalibNUM(:,16:23),aqCalibPM(:,1)-aqCalibPM(:,2)); %PM10
+
+Dens(1:11)=D(3);
+Dens(12:15)=D(2);
+Dens(16:23)=D(1);
 Mass=((4*pi/3)*(Diam./2).^3).*Dens/1000000;
 
 PM1mod=aqCalibNUM(:,1:11)*Mass(1:11);
 PM25mod=aqCalibNUM(:,1:15)*Mass(1:15);
 PM10mod=aqCalibNUM(:,1:23)*Mass(1:23);
 
-plot(PM10mod,aqCalibPM(:,1),'o')
+plot(PM10mod,aqCalibPM(:,1),'o');
 save('aqCalibMod.mat','PM1mod','PM25mod','PM10mod');
 
 end
+function fit=ObjFnc(Dens,Diam,PMnum,PMobs)
+    Mass=((4*pi/3)*(Diam./2).^3).*Dens/1000000;
+    PMmod=PMnum*Mass;
+%     cost_func = 'NMSE';
+%     fit = 1-goodnessOfFit(PMmod,PMobs,cost_func);
+    cost_func = 'MSE';
+    fit = goodnessOfFit(PMmod,PMobs,cost_func);
+    %fit=sqrt(sum((PMobs-PMmod).^2/std(PMobs)^2));
+end
+function [D,err]=getD(Dens,Diam,NUM,PM)
+    err=ObjFnc(Dens,Diam,NUM,PM);
+    f = @(D)ObjFnc(D,Diam,NUM,PM);
+    lb = ones(length(Dens),1)*1500;
+    ub = ones(length(Dens),1)*3000;
+    A = [];
+    b = [];
+    Aeq = [];
+    beq = [];
+    nonlcon = [];
+    options = psoptimset('MaxIter',3000);
+    [D,err]=patternsearch(f,2340,A,b,Aeq,beq,lb,ub,nonlcon,options);
+end
+function [idxo prtA]=randDivide(M,K) 
+    [n,m]=size(M);
+    np=(n-rem(n,K))/K;
+    B=M;
+    [c,idx]=sort(rand(n,1));
+    C=M(idx,:);
+    i=1;
+    j=1;
+    ptrA={};
+    idxo={};
+    n-mod(n,K)
+    while i<n-mod(n,K)
+        prtA{j}=C(i:i+np-1,:);
+        idxo{i}=idx(i:i+np-1,1);
+        i=i+np;
+        j=j+1;
+    end
+    prtA{j}=C(i:n,:);
+end 
--- R.R
+++ R.R
@@ -1,8 +1,10 @@
 Sys.setenv(TZ = "Etc/GMT-3")
+setwd("~/MATLAB/PMCalibration")
 library("openair", lib.loc="~/R/win-library/3.4")
 library("reshape", lib.loc="~/R/win-library/3.4")
 library("reshape2", lib.loc="~/R/win-library/3.4")
 library("R.matlab", lib.loc="~/R/win-library/3.4")
+library("RDCOMClient", lib.loc="~/R/win-library/3.4")
 
 # #READ METEOROLOGY
 # # Import data from NCDC for AIRPORT (411710 and QU 411700)
@@ -20,16 +22,17 @@ library("R.matlab", lib.loc="~/R/win-library/3.4")
 # NCDC.DIA<-rename(NCDC.DIA,c(ws='wsA',wd='wdA'))
 
 #PREPARE BG DATA
-fileN<-"h:/MyDocs/@PROJECTS/PJ.AAK C7290 NPRP 7-649-2-241 Fugitive/Data/AQERT 2 - June 1-5 OutDoor Home for Calibration.txt"
+fileN<-"AQERT 2 - June 1-5 OutDoor Home for Calibration.txt"
 aqCalib<-import(fileN, date = "Datum", date.format = "%d.%m.%Y %H:%M",header.at=2,data.at=700,sep=";",dec=".",stringsAsFactors = FALSE)
 drops<-c("AK", "NA")
 aqCalib<-aqCalib[,!(names(aqCalib) %in% drops)]
 aqCalib<-rename(aqCalib,c(W.Speed="ws",W.Direct="wd", PM10="PM10.obs",PM2.5="PM25.obs",PM1="PM1.obs"))
+aqCalib<-subset(aqCalib,PM10.obs<500)
 Conc<-aqCalib[,14:36]
 Conc<-data.matrix(Conc)#particles / lt
 PM<-aqCalib[,2:4]
 PM<-data.matrix(PM)#ug/m3
-writeMat(con="P:/Chemical Engineering/Air_Quality_Eng_R_Team/!GITHUB/PMCalibration/aqCalib.mat", aqCalibNUM=Conc,aqCalibPM=PM)
+writeMat(con="aqCalib.mat", aqCalibNUM=Conc,aqCalibPM=PM)
 
 
 
@@ -41,10 +44,9 @@ Diam<-c(0.265,0.290,0.325,0.375,0.425,0.475,0.540,0.615,0.675,0.750,
 Vol<-(4/3)*pi*(Diam/2)^3
 Mass<-Vol # *2500 #density in kg/m3
 Conc<-sweep(Conc,MARGIN=2,Mass/1000000,'*') #ìg/m3
-aqCalib$PM1.mod<-rowSums (Conc[,1:11]*2750, na.rm = FALSE)
-aqCalib$PM25.mod<-aqCalib$PM1.mod+rowSums (Conc[,12:15]*2850, na.rm = FALSE)
-aqCalib$PM10.mod<-aqCalib$PM25.mod+rowSums (Conc[,1:23]*2180, na.rm = FALSE)
-
+aqCalib$PM1.mod<-rowSums (Conc[,1:11]*2727, na.rm = FALSE)
+aqCalib$PM25.mod<-aqCalib$PM1.mod+rowSums (Conc[,12:15]*2765, na.rm = FALSE)
+aqCalib$PM10.mod<-aqCalib$PM25.mod+rowSums (Conc[,16:23]*2158, na.rm = FALSE)
 rm(Conc)
 #write.csv(aqBG, file = "p:/Chemical Engineering/Air_Quality_Eng_R_Team/Papers/00 PM Construction Hala/aqBG.txt")
 
@@ -59,12 +61,12 @@ rm(Conc)
 
 #aqCalib.Study<-subset(aqCalib,date>='2017-05-31 12:00:00' & date<='2017-06-06 00:00:00')
 
-# Get Matlab Data
-aqCalibMod <- readMat('P:/Chemical Engineering/Air_Quality_Eng_R_Team/!GITHUB/PMCalibration/aqCalibMod.mat')
-aq<-merge(aqCalib,aqCalibMod)
+
 
 # Plot Data
 polarPlot(aqCalib,pollutant='PM1.obs',statistic="mean", offset = 50, ws.int = 30, trans = TRUE,grid.line=2,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))                                                                           
+polarPlot(aqCalib,pollutant='PM25.obs',statistic="mean", offset = 50, ws.int = 30, trans = TRUE,grid.line=2,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))                                                                           
+polarPlot(aqCalib,pollutant='PM10.obs',statistic="mean", offset = 50, ws.int = 30, trans = TRUE,grid.line=2,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))                                                                           
 # conditionalEval(aqCalib, obs = "PM10.obs", mod = "PM10.mod",statistic = "ws")
 conditionalEval(aqCalib, obs = "PM10.obs", mod = "PM10.mod")
 conditionalEval(aqCalib, obs = "PM25.obs", mod = "PM25.mod")
@@ -72,54 +74,68 @@ conditionalEval(aqCalib, obs = "PM1.obs", mod = "PM1.mod")
 scatterPlot(aqCalib, x = "PM10.mod", y = "PM10.obs", method = "hexbin", col= "jet", linear=TRUE,mod.line = TRUE)
 scatterPlot(aqCalib, x = "PM25.mod", y = "PM25.obs", method = "hexbin", col= "jet", linear=TRUE,mod.line = TRUE)
 scatterPlot(aqCalib, x = "PM1.mod", y = "PM1.obs", method = "hexbin", col= "jet", linear=TRUE,mod.line = TRUE)
+
+# Get Matlab Calculated Valies
+aqCalibMod <- readMat('aqCalibMod.mat')
+aqCalib$PM10.mat<-aqCalibMod$PM10mod
+aqCalib$PM25.mat<-aqCalibMod$PM25mod
+aqCalib$PM1.mat<-aqCalibMod$PM1mod
+## Confirm matlab calculations 
+scatterPlot(aqCalib, x = "PM10.mod", y = "PM10.mat", method = "scatter", col= "jet", linear=TRUE,mod.line = TRUE)
+scatterPlot(aqCalib, x = "PM25.mod", y = "PM25.mat", method = "scatter", col= "jet", linear=TRUE,mod.line = TRUE)
+scatterPlot(aqCalib, x = "PM1.mod", y = "PM1.mat", method = "scatter",col= "jet", linear=TRUE,mod.line = TRUE)
+## Evaluate matlab calculations 
+scatterPlot(aqCalib, x = "PM10.mat", y = "PM10.obs", method = "hexbin", col= "jet", linear=TRUE,mod.line = TRUE)
+scatterPlot(aqCalib, x = "PM25.mat", y = "PM25.obs", method = "hexbin", col= "jet", linear=TRUE,mod.line = TRUE)
+scatterPlot(aqCalib, x = "PM1.mat", y = "PM1.obs", method = "hexbin",col= "jet", linear=TRUE,mod.line = TRUE)
+
                 
-                
-# #PREPARE CS DATA
-# fileN2<-"p:/Chemical Engineering/Air_Quality_Eng_R_Team/Papers/00 PM Construction Hala/PRose-AQERT2.csv"
-# aqCS<-import(fileN2, date = "date", file.type = "csv",date.format = "%Y-%m-%d %H:%M:%S",header.at=1,dec = ".",sep=";")
-# #polarPlot(subset(aqCS, ws<18),pollutant='PM10',statistic="mean", grid.line=2,k=50,uncertainty=FALSE)
-
-# Merge with Meteorology
-aqCS.NCDC<-merge(aqCS,NCDC.QU, by ="date",all=TRUE)
-aqCS.NCDC<-rename(aqCS.NCDC,c(PM10='PM10_calc'))
-aqCS.NCDC$site<-'Construction Site'
-
-# Filter data for study period
-aqCS.NCDC.Study<-subset(aqCS.NCDC,date>='2014-04-30 11:00:00' & date<='2014-05-08 08:00:00')
-
-
-# PREPARE FOR PLOTTING
-pmLevel<-500
-
-# Filtered histograms, Filtering of the data (as per Hala's excel)
-dFilter2<-subset(aqCS,(wd>=345 | wd<=75))
-dFilter<-subset(aqCS,(wd>=285 | wd<=75))
-dFilterOut<-subset(aqCS,(wd>75 & wd<285))
-dFilter2$filter<-'345-75'
-dFilter$filter<-'285-75'
-dFilterOut$filter<-'75-285'
-pm10Filters<-rbind(dFilter2,dFilter,dFilterOut)
-
-#ggplot(subset(pm10Filters,PM10<pmLevel),aes(PM10,fill=filter))+geom_density(alpha=0.2)
-
-# Multiple plotting for results interpretting
-
-#polarFreq(subset(aqCS.NCDC.Study,PM10_calc<pmLevel),pollutant='PM10_calc',statistic="weighted.mean", offset = 20, ws.int = 2, trans = FALSE,grid.line=2)
-polarPlot(subset(aqCS.NCDC.Study,PM10_calc<3500 & ws<18),pollutant='PM10_calc',statistic="mean", offset = 50, ws.int = 30, trans = TRUE,grid.line=2,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))                                                                           
-#polarCluster(subset(aqCS.NCDC.Study,PM10_calc<1500),pollutant='PM10_calc',n.clusters = 3,k=50)
-
-#polarPlot(subset(aqBG.NCDC.Study,PM10_calc<15000 & ws<18),pollutant='PM10_calc',statistic="mean", grid.line=2,k=10,uncertainty=FALSE,min.bin=3,par.settings=list(fontsize=list(text=24)))
-polarPlot(subset(aqBG.NCDC.Study,PM10_calc<500 & ws<18),pollutant='PM10_calc',statistic="mean", grid.line=2,min.bin=1,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))
-
-#polarFreq(subset(aqBG.NCDC.Study,PM10_calc<5000),pollutant='PM10_calc',statistic="mean", offset = 50, ws.int = 30, trans = FALSE,grid.line=2)
-#percentileRose(subset(aqBG.NCDC.Study,ws<8. & PM10_calc<170), pollutant = "PM10_calc",smooth = FALSE)
-
-# join the two sites
-aqCS.NCDC.Study<-rename(aqCS.NCDC.Study,c(ws='wsCS',wd='WdCS',wsQ='ws',wdQ='wd'))
-t1<-subset(aqCS.NCDC.Study,select=c('ws','wd','site','PM10_calc'),na.rm=TRUE)
-t2<-subset(aqBG.NCDC.Study,select=c('ws','wd','site','PM10_calc'),na.rm=TRUE)
-aq.NCDC.study<-rbind(t1,t2)
-rm(t1,t2)
-#polarPlot(subset(aq.NCDC.study,PM10_calc<15000 & ws<18),pollutant='PM10_calc',statistic="mean", grid.line=2,k=10,uncertainty=FALSE,type='site')
-#percentileRose(subset(aq.NCDC.study,ws<18. & PM10_calc<500), pollutant = "PM10_calc",smooth = FALSE,type='site',angle=11.25)
-polarFreq(subset(aq.NCDC.study,PM10_calc<1500 & ws<18),pollutant='PM10_calc',ws.int = 2,statistic="weighted.mean", grid.line=2,type='site',par.settings=list(fontsize=list(text=18)))
+# # #PREPARE CS DATA
+# # fileN2<-"p:/Chemical Engineering/Air_Quality_Eng_R_Team/Papers/00 PM Construction Hala/PRose-AQERT2.csv"
+# # aqCS<-import(fileN2, date = "date", file.type = "csv",date.format = "%Y-%m-%d %H:%M:%S",header.at=1,dec = ".",sep=";")
+# # #polarPlot(subset(aqCS, ws<18),pollutant='PM10',statistic="mean", grid.line=2,k=50,uncertainty=FALSE)
+# 
+# # Merge with Meteorology
+# aqCS.NCDC<-merge(aqCS,NCDC.QU, by ="date",all=TRUE)
+# aqCS.NCDC<-rename(aqCS.NCDC,c(PM10='PM10_calc'))
+# aqCS.NCDC$site<-'Construction Site'
+# 
+# # Filter data for study period
+# aqCS.NCDC.Study<-subset(aqCS.NCDC,date>='2014-04-30 11:00:00' & date<='2014-05-08 08:00:00')
+# 
+# 
+# # PREPARE FOR PLOTTING
+# pmLevel<-500
+# 
+# # Filtered histograms, Filtering of the data (as per Hala's excel)
+# dFilter2<-subset(aqCS,(wd>=345 | wd<=75))
+# dFilter<-subset(aqCS,(wd>=285 | wd<=75))
+# dFilterOut<-subset(aqCS,(wd>75 & wd<285))
+# dFilter2$filter<-'345-75'
+# dFilter$filter<-'285-75'
+# dFilterOut$filter<-'75-285'
+# pm10Filters<-rbind(dFilter2,dFilter,dFilterOut)
+# 
+# #ggplot(subset(pm10Filters,PM10<pmLevel),aes(PM10,fill=filter))+geom_density(alpha=0.2)
+# 
+# # Multiple plotting for results interpretting
+# 
+# #polarFreq(subset(aqCS.NCDC.Study,PM10_calc<pmLevel),pollutant='PM10_calc',statistic="weighted.mean", offset = 20, ws.int = 2, trans = FALSE,grid.line=2)
+# polarPlot(subset(aqCS.NCDC.Study,PM10_calc<3500 & ws<18),pollutant='PM10_calc',statistic="mean", offset = 50, ws.int = 30, trans = TRUE,grid.line=2,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))                                                                           
+# #polarCluster(subset(aqCS.NCDC.Study,PM10_calc<1500),pollutant='PM10_calc',n.clusters = 3,k=50)
+# 
+# #polarPlot(subset(aqBG.NCDC.Study,PM10_calc<15000 & ws<18),pollutant='PM10_calc',statistic="mean", grid.line=2,k=10,uncertainty=FALSE,min.bin=3,par.settings=list(fontsize=list(text=24)))
+# polarPlot(subset(aqBG.NCDC.Study,PM10_calc<500 & ws<18),pollutant='PM10_calc',statistic="mean", grid.line=2,min.bin=1,k=10,uncertainty=FALSE,par.settings=list(fontsize=list(text=24)))
+# 
+# #polarFreq(subset(aqBG.NCDC.Study,PM10_calc<5000),pollutant='PM10_calc',statistic="mean", offset = 50, ws.int = 30, trans = FALSE,grid.line=2)
+# #percentileRose(subset(aqBG.NCDC.Study,ws<8. & PM10_calc<170), pollutant = "PM10_calc",smooth = FALSE)
+# 
+# # join the two sites
+# aqCS.NCDC.Study<-rename(aqCS.NCDC.Study,c(ws='wsCS',wd='WdCS',wsQ='ws',wdQ='wd'))
+# t1<-subset(aqCS.NCDC.Study,select=c('ws','wd','site','PM10_calc'),na.rm=TRUE)
+# t2<-subset(aqBG.NCDC.Study,select=c('ws','wd','site','PM10_calc'),na.rm=TRUE)
+# aq.NCDC.study<-rbind(t1,t2)
+# rm(t1,t2)
+# #polarPlot(subset(aq.NCDC.study,PM10_calc<15000 & ws<18),pollutant='PM10_calc',statistic="mean", grid.line=2,k=10,uncertainty=FALSE,type='site')
+# #percentileRose(subset(aq.NCDC.study,ws<18. & PM10_calc<500), pollutant = "PM10_calc",smooth = FALSE,type='site',angle=11.25)
+# polarFreq(subset(aq.NCDC.study,PM10_calc<1500 & ws<18),pollutant='PM10_calc',ws.int = 2,statistic="weighted.mean", grid.line=2,type='site',par.settings=list(fontsize=list(text=18)))
